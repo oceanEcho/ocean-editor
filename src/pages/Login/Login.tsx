8884c20551d2bb77879c000d078e50e836325c8b
Ivan Zaitsev <ivan_zaitsev@epam.com>
2019-12-23 13:03:10 +0400

    Fix app colors

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,15 +4,13 @@ import styles from './Login.module.scss';
 
 import { Main } from '../../components/Layout/Main';
 import { LoginForm } from './LoginForm';
-import { Footer } from '../../components/Layout/Footer';
 
 export const Login: FunctionComponent<{}> = () => {
   return (
     <>
       <Main className={styles.main}>
-        <LoginForm className={styles.form}/>
+        <LoginForm className={styles.form} />
       </Main>
-      <Footer />
     </>
   );
 };
